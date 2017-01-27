e8f6d5a487eeeebccbd6bbd05ce3eb8a8a73d70a
Morgan Williams <morgan.williams@bison.howard.edu>
2017-01-27 14:20:09 -0500

    Added comments to the functions.

--- fp.rkt
+++ fp.rkt
@@ -1,17 +1,20 @@
+;; Question 1
+;; Reversed version of L. 
 (define (reverse-general L)
-  (cond((null? L) '())
-       ((list? L)
-        `(append (reverse-general (cdr L))
-                (list (reverse-general (car L)))
+  (cond((null? L) '()) ;; checks for an empty list
+       ((list? L) ;; checks for a list 
+        `(append (reverse-general (cdr L)) ;; recursive function
+                (list (reverse-general (car L))) ;; recursive function 
          )
        )
        (else L)
   )
 )
-
+;; Question 2
+;; Sum of the numbers not in the nested list. 
 (define (sum-up-numbers-simple L)
-  (cond((list? L)0)
-   ((+ L)    
+  (cond((list? L)0) ;; checks for a list 
+   ((+ L) ;; adds the atoms in the list L     
    )
   )
 )
